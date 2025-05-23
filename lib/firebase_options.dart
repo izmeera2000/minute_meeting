d19b522a96680fa5dbd05d52bc2f85a2b8403017
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-05-24 02:14:45 +0800

    as
    dasd

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -56,7 +56,7 @@ class DefaultFirebaseOptions {
     apiKey: 'AIzaSyCR85pGQsRO0w-Eq9MGdVpJU1Ge_BZZ8DQ',
     appId: '1:735529869051:android:8fa91e8e1ace8cb1755a5e',
     messagingSenderId: '735529869051',
-    projectId: 'minute-meeting-188ae',
+    projectId: 'minute-meeting-188ae', 
     storageBucket: 'minute-meeting-188ae.firebasestorage.app',
   );
 }
